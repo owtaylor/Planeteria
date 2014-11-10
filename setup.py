84c8cd29768b5b5c3abd05c2f4a3fb53b130d944
Owen W. Taylor <otaylor@fishsoup.net>
2014-11-10 16:37:53 -0500

    Add setup.py and requirements.txt
    Add information about the application needed for automatic
    installation of dependencies.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Planeteria',
+      version='0.3',
+      description='Hosted reader for blogs',
+      author='James Vasile',
+      author_email='james at hackervisions dot net',
+      url='https://github.com/jvasile/Planeteria',
+#      install_requires=['Django>=1.3'],
+     )
